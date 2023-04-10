5f888be4d8b7fcfa552ef03124a98329a9211013
galaxy821 <hoshion208@naver.com>
2023-04-11 00:56:53 +0900

    Gallery - add store logic downloaded not synced image

--- app/src/main/java/com/lovestory/lovestory/module/GetIamgeById.kt
+++ app/src/main/java/com/lovestory/lovestory/module/GetIamgeById.kt
@@ -1,10 +1,14 @@
 package com.lovestory.lovestory.module
 
+import android.content.ContentValues
 import android.content.Context
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import com.lovestory.lovestory.database.PhotoDatabase
 import com.lovestory.lovestory.network.getNotSyncImage
@@ -12,31 +16,48 @@ import com.lovestory.lovestory.repository.PhotoRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.ResponseBody
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 
 suspend fun getImageById(context: Context ,token : String, photo_id : String){
+    Log.d("MODULE-getImageById", "호출됨")
     lateinit var repository : PhotoRepository
     val localId = System.currentTimeMillis().toString()
     val destinationFile = File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "$localId.jpg")
 
     CoroutineScope(Dispatchers.IO).launch {
-        val response = getNotSyncImage(token = token, photo_id= photo_id)
+        Log.d("MODULE-getImageById", "파일 다운받기 시도")
+        val response = getNotSyncImage(token, photo_id)
         if (response.isSuccessful) {
-            val inputStream = response.body()?.byteStream()
-            inputStream?.let {
-                val lovestoryFolder = File(context.getExternalFilesDir(null), "lovestory")
-                if (!lovestoryFolder.exists()) {
-                    lovestoryFolder.mkdirs()
-                }
-                val imageFile = File(lovestoryFolder, "$localId.jpg")
-                saveInputStreamToFile(inputStream, imageFile)
-            }
+            Log.d("MODULE-getImageById", "파일 다운받기 성공")
+            val responseBody = response.body()!!
+            val fileName = "image.jpg" // 이미지 파일 이름을 지정하세요.
+            val imageFile = saveImageToLoveStoryFolderQPlus(context, responseBody, localId)
+            Log.d("MODULE-getImageById", "파일 저장 완료 $imageFile")
         } else {
-            Log.e("DownloadImage", "Failed to download image: ${response.errorBody()}")
+            Log.e("ERROR", "Failed to download image")
         }
+//        val response = getNotSyncImage(token = token, photo_id= photo_id)
+//        if (response.isSuccessful) {
+//            val inputStream = response.body()?.byteStream()
+//            inputStream?.let {
+//                val lovestoryFolder = File(context.getExternalFilesDir(null),"lovestory")
+//                Log.d("MODULE-getImageById", "$lovestoryFolder")
+//                Log.d("MODULE-getImageById", "$destinationFile")
+//                if (!lovestoryFolder.exists()) {
+//                    lovestoryFolder.mkdirs()
+//                }
+//                val imageFile = File(lovestoryFolder, "$localId.jpg")
+//
+////                saveMediaFile(context = context, fileName = localId, inputStream = inputStream)
+//                saveInputStreamToFile(inputStream, imageFile)
+//            }
+//        } else {
+//            Log.e("DownloadImage", "Failed to download image: ${response.errorBody()}")
+//        }
 //        if(response.isSuccessful){
 //            val photoDatabase = PhotoDatabase.getDatabase(context)
 //            val photoDao = photoDatabase.photoDao()
@@ -76,6 +97,61 @@ suspend fun getImageById(context: Context ,token : String, photo_id : String){
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.Q)
+suspend fun saveImageToLoveStoryFolderQPlus(
+    context: Context,
+    responseBody: ResponseBody,
+    fileName: String
+): Uri {
+    val contentValues = ContentValues().apply {
+        put(MediaStore.Downloads.DISPLAY_NAME, fileName)
+        put(MediaStore.Downloads.MIME_TYPE, "image/jpeg")
+        put(MediaStore.Downloads.RELATIVE_PATH, "${Environment.DIRECTORY_DOWNLOADS}/lovestory")
+        put(MediaStore.Downloads.IS_PENDING, 1)
+    }
+
+    val resolver = context.contentResolver
+    val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+
+    uri?.let { targetUri ->
+        resolver.openOutputStream(targetUri).use { outputStream ->
+            responseBody.byteStream().use { inputStream ->
+                inputStream.copyTo(outputStream!!)
+            }
+        }
+
+        contentValues.clear()
+        contentValues.put(MediaStore.Downloads.IS_PENDING, 0)
+        resolver.update(targetUri, contentValues, null, null)
+    }
+
+    return uri ?: throw IOException("Failed to create new MediaStore record.")
+}
+
+fun createLoveStoryFolder(context: Context): File {
+    val downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+    val loveStoryFolder = File(downloadsFolder, "lovestory")
+    if (!loveStoryFolder.exists()) {
+        loveStoryFolder.mkdirs()
+    }
+    return loveStoryFolder
+}
+
+suspend fun saveImageToLoveStoryFolder(context: Context, responseBody: ResponseBody, fileName: String): File {
+    val loveStoryFolder = createLoveStoryFolder(context)
+    val imageFile = File(loveStoryFolder, fileName)
+    val inputStream = responseBody.byteStream()
+    val outputStream = FileOutputStream(imageFile)
+
+    inputStream.use { input ->
+        outputStream.use { output ->
+            input.copyTo(output)
+        }
+    }
+
+    return imageFile
+}
+
 
 private fun saveInputStreamToFile(inputStream: InputStream, outputFile: File) {
     try {
@@ -89,5 +165,42 @@ private fun saveInputStreamToFile(inputStream: InputStream, outputFile: File) {
 
     } catch (e: IOException) {
         Log.e("SaveImage", "Failed to save image: $e")
+    }
+}
+
+
+@RequiresApi(Build.VERSION_CODES.Q)
+fun saveMediaFile(context: Context, fileName: String, inputStream: InputStream): Boolean {
+
+    try {
+        Log.d("MODULE-saveMediaFile", "이미지 저장 시작")
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/*");
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 1)
+
+        val contentResolver = context.contentResolver
+        val collectionUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        val fileDescriptor = collectionUri?.let { contentResolver.openFileDescriptor(it, "w", null) }
+            ?: return false
+        val fileOutputStream = FileOutputStream(fileDescriptor.fileDescriptor)
+        val buffer = ByteArray(1024)
+        var read: Int
+        while (inputStream.read(buffer).also { read = it } != -1) {
+            fileOutputStream.write(buffer, 0, read)
+        }
+        inputStream.close()
+        fileOutputStream.close()
+        fileDescriptor.close()
+
+        contentValues.clear()
+        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+        contentResolver.update(collectionUri, contentValues, null, null)
+        Log.d("MODULE-saveMediaFile", "이미지 저장 성공")
+        return true
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return false
     }
 }
